0d8bca4de73c685eda8ac81c9922c7ba543ba2a8
Ashley Baker <bakerashley56@gmail.com>
2021-09-22 13:07:28 -0700

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { Switch,  Route } from "react-router-dom";
 import Registration from './views/Registration';
-<<<<<<< HEAD
 import Home from './views/userHome';
 import NewCharacter from './views/newCharacter';
-=======
-import Home from './views/home';
->>>>>>> d340027e548c93e54f4c2934dbd571f0bdccd090
 
 function App() {
   return (
