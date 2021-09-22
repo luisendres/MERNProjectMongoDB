1d3cdacc7eadef15ba6970d922a4fa8355599ba8
Ashley Baker <bakerashley56@gmail.com>
2021-09-22 10:49:10 -0700

    added font family to entire app

---
+++ client/src/views/Reg.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/navbar";
+
+const Registration = (props) => {
+
+
+    return (
+        <Navbar />
+    )
+}
+
+export default Registration;
