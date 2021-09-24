2e5287873dffba4dff850660945819d69dbeb332
Ashley Baker <bakerashley56@gmail.com>
2021-09-24 13:43:24 -0700

    updated details

--- client/src/views/characterDetail.js
+++ client/src/views/characterDetail.js
@@ -27,35 +27,35 @@ const CharacterDetails = (props) => {
         <div className="d-flex justify-content-center">
             <table className="table table-bordered w-75 border border-dark border-2 bg-light">
                 <tbody>
-                    <tr className="bg-secondary">
-                        <td>Character Name:</td>
-                        <td>Player Name:</td>
-                        <td>Faction:</td>
+                    <tr className="bg-secondary fw-bold">
+                        <td colspan="2">Character Name:</td>
+                        <td colspan="2">Player Name:</td>
+                        <td colspan="2">Faction:</td>
                     </tr>
-                    <tr>
-                        <td>{character.Name}</td>
-                        <td>{character.PlayerName}</td>
-                        <td>{character.Faction}</td>
+                    <tr className="fw-bold">
+                        <td colspan="2">{character.Name}</td>
+                        <td colspan="2">{character.PlayerName}</td>
+                        <td colspan="2">{character.Faction}</td>
                     </tr>
-                    <tr className="bg-secondary">
-                        <td colSpan="3">Subfactions: <em>(Tribe, Breed, Clan, Auspice, Legion, Guild, Ghoul, Kinfolk, Path)</em></td>
+                    <tr className="bg-secondary fw-bold">
+                        <td colSpan="6">Subfactions: <em>(Tribe, Breed, Clan, Auspice, Legion, Guild, Ghoul, Kinfolk, Path)</em></td>
                     </tr>
-                    <tr>
-                        <td>{character.Subfaction1}</td>
-                        <td>{character.Subfaction2}</td>
-                        <td>{character.Subfaction3}</td>
+                    <tr className="fw-bold">
+                        <td colspan="2">{character.Subfaction1}</td>
+                        <td colspan="2">{character.Subfaction2}</td>
+                        <td colspan="2">{character.Subfaction3}</td>
                     </tr>
-                    <tr className="bg-secondary">
-                        <td>Patron:</td>
-                        <td>Gen/Rank:</td>
-                        <td>Passion:</td>
+                    <tr className="bg-secondary fw-bold">
+                        <td colspan="2">Patron:</td>
+                        <td colspan="2">Gen/Rank:</td>
+                        <td colspan="2">Passion:</td>
                     </tr>
-                    <tr>
-                        <td>{character.Patron}</td>
-                        <td>{character.GenRank}</td>
-                        <td>{character.Passion}</td>
+                    <tr className="fw-bold">
+                        <td colspan="2">{character.Patron}</td>
+                        <td colspan="2">{character.GenRank}</td>
+                        <td colspan="2">{character.Passion}</td>
                     </tr>
-                    <tr className="bg-secondary">
+                    <tr className="bg-secondary fw-bold">
                         <td>Health:</td>
                         <td>Willpower:</td>
                         <td>{character.EnergyType}:</td>
@@ -63,9 +63,12 @@ const CharacterDetails = (props) => {
                         <td>Devoured:</td>
                         <td>Shadow - Deed Name - Sire</td>
                     </tr>
-                    <tr></tr>
+                    <tr className="fw-bold">
+                        <td>{character.Health}</td>
+                        <td>{character.Willpower}</td>
+                        <td>{}</td>
+                    </tr>
                 </tbody>
-
             </table>
         </div>
     )
